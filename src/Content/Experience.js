b4fb9e84ac4dff174db0ebb336e21f8b5e29538c
Rajat Maheshwari <54249328+rajatmaheshwari2512@users.noreply.github.com>
2023-07-22 14:20:51 +0530

    Added D.E. Shaw

--- src/Content/Experience.js
+++ src/Content/Experience.js
@@ -1,75 +1,87 @@
 export const ExperienceContent = [
   {
-    company: "PrepiiT",
-    link: "https://testprepiit.ml",
-    role: "Backend Developer",
+    company: 'D.E. Shaw India',
+    link: 'https://www.deshawindia.com/',
+    role: 'Systems Intern',
     brief: [
-      "Developed the backend of the PrepiiT Quizzing Platform",
-      "Developed a RESTful API in NodeJS, and the framework used was ExpressJS",
-      "Used PostgreSQL as Database of choice",
-      "Everytime a user wants to give a test, questions are randomly picked from the Database according to the paper pattern set by the admin",
-      "Admin can add, remove, edit all questions and tests, bulk questions can be uploaded in CSV format",
-      "All users get their test analysis which is all handled in the backend",
-      "Live Testing feature is also available (Test will be available only for certain amount of time)",
-    ],
+      'Developed a real-time dashboard to visualize Windows server maintenance',
+      'Utilized ReactJS, MaterialUI, .NET Core 7, and Microsoft SQL Serverto create an intuitive and informative dashboard',
+      'Reduced system latency from over 8 seconds to less than 250 milliseconds',
+      'Implemented a feature providing patching operators with comprehensive timelines of server states, facilitating precise issue identification',
+      'Used Axios for API Calls to create, delete and update databases',
+      'Miscellaneously, developed a bash script for automating Vite application deployment on Kubernetes, streamlining the deployment process'
+    ]
   },
   {
-    company: "iMocha",
-    link: "https://imocha.com",
-    role: "MVP & POC Intern",
+    company: 'AI4Bharat, IIT Madras',
+    link: 'https://ai4bharat.org',
+    role: 'Front-End Developer',
     brief: [
-      "Worked at iMocha as a Full Stack Development intern",
-      "Worked on extending the current functionality of the skill assessment platform of iMocha",
-      "Used a browser based IDE, Eclipse Theia, to create a development skill assessment POC project",
-      "Users can login, with a particular test link, the details of the test, which are set by the admin are then loaded in.",
-      "A Docker container is created for the user to attempt his assessment in. A folder structure is displayed, the user can create as many files and folders as he needs.",
-      "Buttons for running and stopping the service without touching the terminal is also implemented, although the user can use the terminal is he wants to",
-      "On submission, the server will zip the submission, and push it to a persistent storage like Amazon S3 or Microsoft Azure. The admin who created the test can then view submissions and download them according to his liking",
-      "The Theia workspace comes with a browser preview extension built into it, the user can run the project on any port he likes and view the project freely",
+      'Worked as a Frontend Developer, at AI4Bharat, IIT Madras',
+      'AI4Bhārat is a non-profit, open-source community of engineers, domain experts, policy makers, and academicians collaborating to build AI solutions to solve India’s problems',
+      'Refactored an existing project (Subplayer.js), to support various new features like direct translation from English typing to Hindi, and a new feature to support multiple vernacular languages',
+      'Built using React, Redux, and Ant Design',
+      'Worked on an in-house project Shoonya, that has functionalities to undertake a project, for collecting or annotating data',
+      'Created a workflow to create Annotation Projects with dynamic forms wherein progressive inputs change according to the previous selections',
+      'Worked on displaying the Projects page, and add new members to the project as well',
+      'Used extensively for captioning purposes in NPTEL videos, which is the largest provider of MOOC courses in India'
     ],
+    youtubeLink: 'https://www.youtube.com/watch?v=Jq3CcEb9pxQ'
   },
   {
-    company: "FOSSEE, IIT Bombay",
-    link: "https://github.com/frg-fossee/eSim-Cloud",
-    role: "Full Stack Developer",
+    company: 'Horizon Technologies',
+    link: 'https://spinup.host',
+    role: 'Front-End Developer and Maintainer',
     brief: [
-      "Worked as a Full Stack Developer, at FOSSEE, IIT Bombay",
-      "Developed a Version Control System, for users to save multiple versions of a circuit, and to revert back to previous versions, giving users the freedom to explore various options without the fear of losing progress",
-      "Branching is also supported and the user can create multiple branches according to his needs",
-      "Integrated versioning with workflow to publish various circuits, with the ability to publish various versions of the circuit",
-      "Worked on adding a Popup bar for setting component properties, which allows users to set the parameters of a component right next it.",
-      "This is especially useful as it allows direct integration of the Schematic Editor in various places without the need to add the entire page.",
-    ],
-    report:
-      "https://drive.google.com/file/d/14oGikIo7GG-gcybT_ee_McOl1pjUvAGO/view?usp=sharing",
+      'Worked as a Frontend Stack Developer, at Spinup, Horizon Technologies',
+      'Spinup is an Open Source AWS RDS alternative',
+      'Users can register on GitHub, select a type of database, and use it according to their needs',
+      'Built using React, Redux, and Ant Design',
+      'Used Axios for API Calls to create, delete and update databases',
+      'A Profile page for customising each cluster is also implemented'
+    ]
   },
   {
-    company: "Horizon Technologies",
-    link: "https://spinup.host",
-    role: "Front-End Developer and Maintainer",
+    company: 'FOSSEE, IIT Bombay',
+    link: 'https://github.com/frg-fossee/eSim-Cloud',
+    role: 'Full Stack Developer',
     brief: [
-      "Worked as a Frontend Stack Developer, at Spinup, Horizon Technologies",
-      "Spinup is an Open Source AWS RDS alternative",
-      "Users can register on GitHub, select a type of database, and use it according to their needs",
-      "Built using React, Redux, and Ant Design",
-      "Used Axios for API Calls to create, delete and update databases",
-      "A Profile page for customising each cluster is also implemented",
+      'Worked as a Full Stack Developer, at FOSSEE, IIT Bombay',
+      'Developed a Version Control System, for users to save multiple versions of a circuit, and to revert back to previous versions, giving users the freedom to explore various options without the fear of losing progress',
+      'Branching is also supported and the user can create multiple branches according to his needs',
+      'Integrated versioning with workflow to publish various circuits, with the ability to publish various versions of the circuit',
+      'Worked on adding a Popup bar for setting component properties, which allows users to set the parameters of a component right next it.',
+      'This is especially useful as it allows direct integration of the Schematic Editor in various places without the need to add the entire page.'
     ],
+    report: 'https://drive.google.com/file/d/14oGikIo7GG-gcybT_ee_McOl1pjUvAGO/view?usp=sharing'
   },
   {
-    company: "AI4Bharat, IIT Madras",
-    link: "https://ai4bharat.org",
-    role: "Front-End Developer",
+    company: 'iMocha',
+    link: 'https://imocha.com',
+    role: 'MVP & POC Intern',
     brief: [
-      "Worked as a Frontend Developer, at AI4Bharat, IIT Madras",
-      "AI4Bhārat is a non-profit, open-source community of engineers, domain experts, policy makers, and academicians collaborating to build AI solutions to solve India’s problems",
-      "Refactored an existing project (Subplayer.js), to support various new features like direct translation from English typing to Hindi, and a new feature to support multiple vernacular languages",
-      "Built using React, Redux, and Ant Design",
-      "Worked on an in-house project Shoonya, that has functionalities to undertake a project, for collecting or annotating data",
-      "Created a workflow to create Annotation Projects with dynamic forms wherein progressive inputs change according to the previous selections",
-      "Worked on displaying the Projects page, and add new members to the project as well",
-      "Used extensively for captioning purposes in NPTEL videos, which is the largest provider of MOOC courses in India",
-    ],
-    youtubeLink: "https://www.youtube.com/watch?v=Jq3CcEb9pxQ",
+      'Worked at iMocha as a Full Stack Development intern',
+      'Worked on extending the current functionality of the skill assessment platform of iMocha',
+      'Used a browser based IDE, Eclipse Theia, to create a development skill assessment POC project',
+      'Users can login, with a particular test link, the details of the test, which are set by the admin are then loaded in.',
+      'A Docker container is created for the user to attempt his assessment in. A folder structure is displayed, the user can create as many files and folders as he needs.',
+      'Buttons for running and stopping the service without touching the terminal is also implemented, although the user can use the terminal is he wants to',
+      'On submission, the server will zip the submission, and push it to a persistent storage like Amazon S3 or Microsoft Azure. The admin who created the test can then view submissions and download them according to his liking',
+      'The Theia workspace comes with a browser preview extension built into it, the user can run the project on any port he likes and view the project freely'
+    ]
   },
+  {
+    company: 'PrepiiT',
+    link: 'https://testprepiit.ml',
+    role: 'Backend Developer',
+    brief: [
+      'Developed the backend of the PrepiiT Quizzing Platform',
+      'Developed a RESTful API in NodeJS, and the framework used was ExpressJS',
+      'Used PostgreSQL as Database of choice',
+      'Everytime a user wants to give a test, questions are randomly picked from the Database according to the paper pattern set by the admin',
+      'Admin can add, remove, edit all questions and tests, bulk questions can be uploaded in CSV format',
+      'All users get their test analysis which is all handled in the backend',
+      'Live Testing feature is also available (Test will be available only for certain amount of time)'
+    ]
+  }
 ];
